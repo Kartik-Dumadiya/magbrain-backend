200ef2fa5b53f8f7c700875c37affe4b94ae1670
Kartik-Dumadiya <kartikdumadiya0111@gmail.com>
2025-06-10 22:58:10 +0530

    All is well

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import flowRoutes from "./routes/flowRoutes.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://magbrain-frontend.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
